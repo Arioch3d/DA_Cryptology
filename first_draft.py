7f0d0c19f5b43e9599e29072e4f29edec31ac083
Angela Moore <angela.rene.moore@gmail.com>
2025-04-04 09:42:10 -0400

    first draft starting encryption program

---
+++ first_draft.py
@@ -0,0 +1,32 @@
+""" 
+This may end up being mostly psuedo code- 
+Just trying to wrap my head around the concepts
+"""
+
+print("Welcome to the affine cypher!")
+
+crypt_type = str(input("Would you like to encrypt or decrypt a message?")).lower
+message = str(input("Please enter a message to encrypt:")).lower
+mult_key = input("Please enter a multiplicative key value (1-26):").lower
+add_key = input("Please enter an additive key value (1-26):").lower
+
+
+# start_code_Dict = {"a": 0, "b": 1, "c": 2, "d": 3}
+
+
+# def encrypt(message):
+#     if crypt_type == "encrypt":
+#         print("Encrypting... please wait.")
+#     elif crypt_type == "decrypt":
+#         print("Decrypting... please wait.")
+    
+#     # for letter in message_letters:
+#     #     encrypted_message = 
+        
+#     encrypted_message = message[::-1]
+    
+#     print(encrypted_message)
+    
+    
+# encrypt(message)
+
