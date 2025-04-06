5eb7c24d082cc5f8deca646fa68fce2f508ba669
Charles Norman <arioch3d@gmail.com>
2025-04-06 16:18:50 -0400

    Continuing testing

--- index.py
+++ index.py
@@ -1,55 +1,71 @@
-def encrypt_cipher(plaintext, key1, key2):
-    text = plaintext
+import string
+
+# Define the alphabet
+ALPHABET = string.ascii_uppercase
+
+def letter_to_index(letter):
+    return ALPHABET.index(letter)
+
+def index_to_letter(index):
+    return ALPHABET[index]
+
+def mod_inverse(a, m):
+    for i in range(1, m):
+        if (a * i) % m == 1:
+            return i
+    return None
+
+def encrypt(plaintext, a, b):
     ciphertext = ""
-    for char in text:
-        if char.isalpha():
-            order = ord(char)
-            if char.islower():
-                order = order - 97
-                order = ((order * key1) + key2) % 26
-                order = order + 97
-                new_char = chr(order)
-                ciphertext = ciphertext + new_char
-            if char.isupper():
-                order = order - 65
-                order = ((order * key1) + key2) % 26
-                order = order + 65
-                new_char = chr(order)
-                ciphertext = ciphertext + new_char
+    for letter in plaintext:
+        if letter in ALPHABET:
+            x = letter_to_index(letter)
+            encrypted_index = (a * x + b) % len(ALPHABET)
+            ciphertext += index_to_letter(encrypted_index)
         else:
-            ciphertext = ciphertext + char
+            ciphertext += letter
     return ciphertext
 
-#z = encrypt_cipher()
-def decrypt_cipher(ciphertext, key1, key2):
-    text = ciphertext
-    #ciphertext = ""
-    for char in text:
-        if char.isalpha():
-            order = ord(char)
-            if char.islower():
-                order = order - 97
-                order = ((order * key2) - key1) % 26
-                order = order + 97
-                new_char = chr(order)
-                plaintext = text + new_char
-            if char.isupper():
-                order = order - 65
-                order = ((order * key2) - key1) % 26
-                order = order + 65
-                new_char = chr(order)
-                plaintext = text + new_char
+def decrypt(ciphertext, a, b):
+    plaintext = ""
+    a_inv = mod_inverse(a, len(ALPHABET))
+    if a_inv is None:
+        return "Error: 'a' is not coprime to 26"
+    for letter in ciphertext:
+        if letter in ALPHABET:
+            y = letter_to_index(letter)
+            decrypted_index = (a_inv * (y - b)) % len(ALPHABET)
+            plaintext += index_to_letter(decrypted_index)
         else:
-            plaintext = text + char
+            plaintext += letter
     return plaintext
 
-plaintext = input("Enter the plaintext: ")
-user_multiplicative = int(input("Enter Multiplicative Key: "))
-user_additive = int(input("Enter Additive Key: "))
+# everything below i did myself, using the 'encrypt' and 'decrypt' functions from the code above that i found online
+print("Welcome to the Affine Cypher!")
 
-decrypted_text = decrypt_cipher(ciphertext, user_multiplicative, user_additive)
-
-print("Entered plaintext: ", plaintext)
-print("Entered keys are: \n Multiplicative Key = ", user_multiplicative, "\n Additive Key = ", user_additive)
-print("Ciphertext : ", ciphertext)
-print("Decrypted text: ", decrypted_text)
+crypt_again = True
+while True:
+    crypt_choice = int(input("Would you like to (1)encrypt or (2)decrypt a message?"))
+    if crypt_choice == 1:
+        plaintext = str(input("Please type the message you would like encrypted:")).upper()
+        a = int(input("Please choose a multiplicative value(1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25):"))
+        b = int(input("Please choose an additive value (1-26):"))
+        cyphertext = encrypt(plaintext, a, b)
+        print(f"Your encrypted message is '{cyphertext}'")
+    elif crypt_choice == 2:
+        cyphertext = str(input("Please enter the message to be decrypted:")).upper()
+        a = int(input("Please choose a multiplicative value(1, 3, 5, 7, 9, 11, 15, 17, 19, 21, 23, 25):"))
+        b = int(input("Please choose an additive value (1-26):"))
+        plaintext = decrypt(cyphertext, a, b)
+        print(f"Your decrypted message is: {plaintext}")
+     
+    
+    crypt_again = input("Would you like to encrypt or decrypt another message? (yes/no)").lower()
+    if crypt_again == "no":
+        another_crypt = False
+        print("Thank you for using the affine cypher.")
+        break
+    elif crypt_again == "yes":
+        continue
+    
+    
